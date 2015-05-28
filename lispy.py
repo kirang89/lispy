7b1d85e942563176278d5f03877244db87319962
kirang89 <me@kirang.in>
2015-05-28 08:58:48 +0530

    Data types, strings, comments(with quirks), quotes, I/O

--- lispy.py
+++ lispy.py
@@ -2,14 +2,19 @@
 # Lispy: A lisp interpreter in Python
 #
 
+import sys
+import re
+import StringIO
+
 LEFT_PAREN = '('
 RIGHT_PAREN = ')'
-QUOTE = 'quote'
-DEFINITION = 'define'
-CONDITIONAL = 'if'
-SYMBOL = str
-SET = 'set!'
-LAMBDA = 'lambda'
+TOKENIZER = r'''\s*(,@|[('`,)]|"(?:[\\].|[^\\"])*"|;.*|[^\s('"`,;)]*)(.*)'''
+TRUE = "#t"
+FALSE = "#f"
+
+
+class Symbol(str):
+    pass
 
 
 class Procedure(object):
@@ -40,6 +45,63 @@ class Env(dict):
             return self.parent.find(var)
 
 
+def Sym(s, symbol_table={}):
+    """
+    Find or create unique symbol entry for 's' in
+    a symbol table
+    """
+    if s not in symbol_table:
+        symbol_table[s] = Symbol(s)
+
+    return symbol_table[s]
+
+
+EOF = Symbol('#<eof-object>')
+
+
+class InPort(object):
+    """Input port.
+
+    Used to retain a line of characters.
+    """
+    def __init__(self, file):
+        self.file = file
+        self.line = ''
+
+    def next_token(self):
+        """Return next token
+
+        As a consequence, new chars will be stored into
+        the line buffer(if needed).
+        """
+        if self.line == '':
+            self.line = self.file.readline()
+
+        if self.line == '':
+            return EOF
+
+        token, self.line = re.match(TOKENIZER, self.line).groups()
+
+        if token and not token.startswith(';'):
+            return token
+
+
+QUOTE, IF, SET, DEFINE, LAMBDA, BEGIN, DEFINEMACRO, = map(
+    Sym, "quote   if   set!  define   lambda   begin   define-macro".split()
+)
+
+QUASIQUOTE, UNQUOTE, UNQUOTESPLICING = map(
+    Sym, "quasiquote   unquote   unquote-splicing".split()
+)
+
+QUOTES = {
+    "'": QUOTE,
+    "`": QUASIQUOTE,
+    ",": UNQUOTE,
+    ",@": UNQUOTESPLICING
+}
+
+
 def standard_env():
     "An environment with some Scheme standard procedures."
     import math
@@ -63,7 +125,7 @@ def standard_env():
         'begin': lambda *x: x[-1],
         'car': lambda x: x[0],
         'cdr': lambda x: x[1:],
-        'cons': lambda x, y: [x] + y,
+        'cons': lambda x, y: x + y,
         'eq?': op.is_,
         'equal?': op.eq,
         'length': len,
@@ -77,7 +139,7 @@ def standard_env():
         'number?': lambda x: isinstance(x, (int, float)),
         'procedure?': callable,
         'round':   round,
-        'symbol?': lambda x: isinstance(x, SYMBOL),
+        'symbol?': lambda x: isinstance(x, Symbol),
     })
     return env
 
@@ -90,27 +152,38 @@ def tokenize(string):
     return string.replace('(', '( ').replace(')', ' )').split()
 
 
-def read_from_tokens(tokens):
-    "Read an expression from a sequence of tokens"
-    if len(tokens) == 0:
-        raise SyntaxError("unexpected EOF while reading")
-
-    token = tokens.pop(0)
-
-    if token == LEFT_PAREN:
-        sub_exp = []
-
-        while tokens[0] != RIGHT_PAREN:
-            sub_exp.append(read_from_tokens(tokens))
-
-        tokens.pop(0)
-        return sub_exp
-
-    elif token == RIGHT_PAREN:
-        raise SyntaxError("unexpected ')'")
-
+def readchar(inport):
+    "Read next character from inport"
+    if inport.line:
+        ch, inport.line = inport.line[0], inport.line[1:]
+        return ch
     else:
-        return atom(token)
+        return inport.file.read(1) or EOF
+
+
+def read(inport):
+    "Read a Scheme expression from input port"
+    def read_ahead(token):
+        if token == LEFT_PAREN:
+            sub_exp = []
+
+            while True:
+                token = inport.next_token()
+                if token == RIGHT_PAREN:
+                    return sub_exp
+                else:
+                    sub_exp.append(read_ahead(token))
+        elif token == RIGHT_PAREN:
+            raise SyntaxError("unexpected ')'")
+        elif token in QUOTES:
+            return [QUOTES[token], read(inport)]
+        elif token == EOF:
+            raise SyntaxError("unexpected eof in list")
+        else:
+            return atom(token)
+
+    token_next = inport.next_token()
+    return EOF if token_next is EOF else read_ahead(token_next)
 
 
 def NUMBER(token):
@@ -118,26 +191,42 @@ def NUMBER(token):
     try:
         return int(token)
     except ValueError:
+        pass
+
+    try:
         return float(token)
+    except ValueError:
+        pass
+
+    return complex(token.replace('i', 'j', 1))
 
 
 def atom(token):
-    "Parse token as a number or a symbol"
+    "Parse token as a boolean/string/number or a symbol"
+    if token == TRUE:
+        return True
+    elif token == FALSE:
+        return False
+    elif token == '"':
+        return token[1:-1].decode('string_escape')
+
     try:
         return NUMBER(token)
     except ValueError:
-        return SYMBOL(token)
+        return Sym(token)
 
 
-def parse(program):
-    "Parse a scheme expression from a string"
-    return read_from_tokens(tokenize(program))
+def parse(inport):
+    "Parse a scheme expression from input port"
+    if isinstance(inport, str):
+        inport = InPort(StringIO.StringIO(inport))
+    return read(inport)
 
 
 def eval(exp, env=global_env):
     "Evaluates a expression in context of \
     an environment"
-    if isinstance(exp, SYMBOL):
+    if isinstance(exp, Symbol):
         return env.find(exp)[exp]
     elif not isinstance(exp, list):
         # Return as is, if not an expression
@@ -145,10 +234,10 @@ def eval(exp, env=global_env):
     elif exp[0] == QUOTE:
         (_, _exp) = exp
         return _exp
-    elif exp[0] == DEFINITION:
+    elif exp[0] == DEFINE:
         (_, variable, value) = exp
         env[variable] = eval(value, env)
-    elif exp[0] == CONDITIONAL:
+    elif exp[0] == IF:
         (_, test, conseq, alt) = exp
         _exp = conseq if eval(test, env) else alt
         return eval(_exp, env)
@@ -167,20 +256,50 @@ def eval(exp, env=global_env):
 
 def schemeify(val):
     "Convert value to a valid scheme string"
-    if isinstance(val, list):
+    if val is True:
+        return '#t'
+    elif val is False:
+        return '#f'
+    elif isinstance(val, Symbol):
+        return val
+    elif isinstance(val, str):
+        val = val.encode('string_escape').replace('"', r'\"')
+        return '"{}"'.format(val)
+    elif isinstance(val, complex):
+        val = str(val).replace('j', 'i')
+        return '"{}"'.format(val)
+    elif isinstance(val, list):
         elems = ' '.join([str(x) for x in val])
         return ''.join(['(', elems, ')'])
     else:
         return str(val)
 
 
-def repl(prompt="lispy> "):
-    "REPL for lispy"
-    while True:
-        input = raw_input(prompt)
-        if input == 'exit':
-            return
+def load(filename):
+    "Eval scheme code from a file"
+    repl(None, InPort(open(filename)), None)
 
-        result = eval(parse(input))
-        if result:
-            print schemeify(result)
+
+def repl(prompt="lispy> ", inport=InPort(sys.stdin), out=sys.stdout):
+    "REPL for lispy"
+    sys.stderr.write("Lispy v1.0\n")
+    while True:
+        try:
+            if prompt:
+                sys.stderr.write(prompt)
+
+            parsed_val = parse(inport)
+            if parsed_val is EOF:
+                return
+
+            result = eval(parsed_val)
+            if result and out:
+                print >> out, schemeify(result)
+        except Exception as e:
+            import traceback
+            print traceback.format_exc()
+            # print "{}: {}".format(type(e).__name__, e)
+
+
+if __name__ == '__main__':
+    repl()
